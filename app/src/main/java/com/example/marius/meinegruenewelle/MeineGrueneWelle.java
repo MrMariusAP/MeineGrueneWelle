a0ef96d2a2740acaafb4c4a42fd6b848a94a4a84
MrMariusAP <mariusmauksch@gmail.com>
2015-06-06 15:12:20 +0200

    Fuer Noah

--- app/src/main/java/com/example/marius/meinegruenewelle/MeineGrueneWelle.java
+++ app/src/main/java/com/example/marius/meinegruenewelle/MeineGrueneWelle.java
@@ -1,17 +1,42 @@
 package com.example.marius.meinegruenewelle;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MeineGrueneWelle extends ActionBarActivity {
+    LocationListener locationListener = new LocationListener() {
+
+        // Wird Aufgerufen, wenn eine neue Position durch den LocationProvider bestimmt wurde
+        public void onLocationChanged(Location location) {
+
+            TextView tv1 = (TextView) MeineGrueneWelle.this.findViewById(R.id.Laengengrad);
+            TextView tv2 = (TextView) MeineGrueneWelle.this.findViewById(R.id.Breitengrad);
+            tv1.setText(String.valueOf(location.getLatitude()));
+            tv2.setText(String.valueOf(location.getLongitude()));
+
+
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meine_gruene_welle);
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 
     @Override
